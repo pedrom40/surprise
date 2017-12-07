1cc894fe5e104544f5b9e01c9b48311f25ae0d91
pedrom40 <pedro.morin1980@gmail.com>
2017-12-07 08:23:37 -0600

    updated code to match thinkful solution

--- src/components/surprise-button.js
+++ src/components/surprise-button.js
@@ -1,20 +1,5 @@
 import React from 'react';
 
-export default class SurpriseButton extends React.Component {
-
-  // initial function run on first render
-  constructor(props) {
-    super(props);
-  }
-
-  // updates the onShowImg props attr to send up to the Surprise component
-  onShowImg(showImg) {
-    this.props.onShowImg(showImg);
-  }
-
-  render() {
-    return <button onClick={ () => this.onShowImg(true)}>Surprise!</button>;
-  }
-
+export default function SurpriseButton(props) {
+  return <button onClick={props.onClick}>Surprise!</button>;
 }
-
--- src/components/surprise.js
+++ src/components/surprise.js
@@ -3,36 +3,25 @@ import SurpriseButton from './surprise-button';
 import SurpriseImage from './surprise-image';
 
 export default class Surprise extends React.Component {
-
-  // initial function run on first render
   constructor(props) {
     super(props);
-
-    // state belongs here because this is where the render will change based on current state
     this.state = {
-      showImg: false
+      display: 'button'
     }
   }
 
-  // this updates the state, triggering a re-rendering of the component
-  setShowImg(showImg) {
+  showImage() {
     this.setState({
-      showImg
-    })
+      display: 'image'
+    });
   }
 
-  // sends the component to the view
   render() {
-
-    // if showImg is true, render image component
-    if (this.state.showImg) {
-      return (
-        <SurpriseImage />
-      );
+    if (this.state.display === 'button') {
+      return <SurpriseButton onClick={e => this.showImage()} />;
     }
-
-    // the onShowImg function is passed up from the surprise button component
-    return <SurpriseButton onShowImg={ showImg => this.setShowImg(showImg)} />;
-
+    else if (this.state.display === 'image') {
+      return <SurpriseImage />;
+    }
   }
 }
